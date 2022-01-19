905e2884d96483d0d6a0e60bef6ae4090202e421
manuel <manuel_hirzel@hotmail.com>
2022-01-19 12:46:37 +0100

    try to fix animated background

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { slideInAnimation } from './animation';
 
@@ -10,10 +10,18 @@ import { slideInAnimation } from './animation';
     slideInAnimation
   ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Hirzel Technology';
-
+  mobile = false;
+  outlet:any
   prepareRoute(outlet: RouterOutlet) {
     return outlet?.activatedRouteData?.['animation'];
+  }
+ 
+  ngOnInit() {
+    if (window.screen.width < 400) { // 768px portrait
+      this.mobile = true;
+    };
+    console.log(this.mobile)
   }
 }
