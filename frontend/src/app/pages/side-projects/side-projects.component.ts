61a9e2ff7a6b7433f53f280a43dfd044843955f0
manuel <manuel_hirzel@hotmail.com>
2022-01-20 17:02:31 +0100

    changed text

--- frontend/src/app/pages/side-projects/side-projects.component.ts
+++ frontend/src/app/pages/side-projects/side-projects.component.ts
@@ -9,7 +9,7 @@ export class SideProjectsComponent implements OnInit {
 
   public infraIsCollapsed = true
   public coronaIsCollapsed = true;
-  public pmIsCollapsed = true;
+  public pmIsCollapsed = false;
   public aqIsCollapsed = true;
 
   constructor() { }
